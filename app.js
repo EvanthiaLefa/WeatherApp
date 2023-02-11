f6fa7b00e9959910c653593b2b37dc81bcfeab32
Evanthia Lefa <euanthialepha@gmail.com>
2023-02-11 13:08:14 +0200

    New Changes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
-const temp =document.querySelector('#temp')
-const feels =document.querySelector('#feels')
-const humid =document.querySelector('#humid')
-const wind =document.querySelector('#wind')
+const temp = document.querySelector('#temp')
+const feels = document.querySelector('#feels')
+const humid = document.querySelector('#humid')
+const wind = document.querySelector('#wind')
+const condition = document.querySelector('#condition')
 const btn = document.querySelector("button");
 const input = document.querySelector("input");
-const loc =document.querySelector('#loc')
+const loc = document.querySelector('#loc')
 const img = document.querySelector('img')
 const p = document.querySelector("p");
-const br = document.createElement('br')
+const br = document.createElement('br');
+const ulDaily = document.querySelector('#ulDaily');
 
 
 let obj;
@@ -18,9 +19,22 @@ function getData() {
     .then(res => res.json())
     .then(data => {
       obj = data;
+      console.log(obj)
       putData()
+      getDailyForecast()
     })
     .catch(error => console.log('Something Went Wrong!', error));
+}
+
+function getDailyForecast() {
+  ulDaily.innerHTML = "";
+  // HERE I TAKE THE FIRST 5 DAYS OF THE ARRAY FOR THE DAILY FORECAST
+  const dailyForecast = obj.days.slice(0, 5);
+  dailyForecast.forEach((el) => {
+    const newLi = document.createElement("li")
+    newLi.innerHTML = ` Date: ${el.datetime} , Temp: ${el.temp} , Max: ${el.tempmax} , Min: ${el.tempmin}`;
+    ulDaily.append(newLi)
+  })
 }
 
 function putData() {
@@ -30,40 +44,20 @@ function putData() {
   windsData = obj.currentConditions.windspeed;
   conditions = obj.currentConditions.conditions;
   address = obj.address;
-  console.log(conditions)
+  console.log(obj.currentConditions.conditions)
 
   temp.innerText = `${tempData}°C`
   feels.innerText = `Feelslike:${feelsData}`
   humid.innerText = `Humidity: ${humidityData}%`
   wind.innerText = `Windspeed: ${windsData}km/h`
+  condition.innerText = `Conditions: ${conditions}`
   loc.innerText = address.toUpperCase()
-  p.innerText = ""
-  let hours = obj.days[0].hours
-  for (let i = 0; i < hours.length; i++) {
-    let newHour = [];
-    let hour = `${hours[i].datetime}-${hours[i].temp}°C`
-    p.innerText += "  " + hour
-  }
   putIcons()
 }
 
-function putIcons(){
-  if(conditions == 'Overcast'){
-    img.src= './icon/cloudy.jpeg'
-    img.style.display = 'block';
-   } else if(conditions =='Clear'){
-     img.src= './icon/clear.jpeg'
-     img.style.display = 'block';
-   }else if(conditions =='Partially cloudy'){
-     img.src= './icon/partialCloudy.jpeg'
-     img.style.display = 'block';
-   }else if(conditions =='Snow, Overcast'){
-     img.src= './icon/snow.jpeg'
-     img.style.display = 'block';
-   }else if(conditions =='Rain, Overcast'){
-     img.src= './icon/rain.jpeg'
-     img.style.display = 'block';
-   }
+function putIcons() {
+  img.src = `./iconSet/${obj.currentConditions.icon}.png`
+  img.style.display = 'block';
 }
 
 btn.addEventListener("click", getData)
